98a7bb833f67700e74d4f300f00bbd0e88b3730f
Felipe Reis <felipe@glr.com.br>
2017-03-04 12:32:32 -0300

    feat: Solve problem 6
    Use of map, reduce and lambda expressions to solve 'Sum square difference' problem.

---
+++ pe006.py
@@ -0,0 +1,28 @@
+'''
+
+Sum square difference
+======================
+
+The sum of the squares of the first ten natural numbers is,
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+'''
+from problem import Problem
+
+class Pe006(Problem):
+
+    def execute(self):
+        nums = range(101)[1:]
+
+        sumOfSquares = reduce(lambda x,y: x + y, map(lambda x: x**2, nums))
+        squareOfSums = reduce(lambda x,y: x+y, nums) ** 2
+
+        return squareOfSums - sumOfSquares
+
+Pe006().main()
